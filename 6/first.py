9abda2b275cace048dc8c3d4b4991260aeb12f1e
Your Name <nathanielbd@gmail.com>
2023-12-05 23:35:00 -0600

    wow why did i decide to make the times a dict key

--- 6/first.py
+++ 6/first.py
@@ -1,2 +1,13 @@
 f = open('input.txt', 'r')
 data = [row.strip() for row in f]
+times = [int(val) for val in data[0].split(':')[1].split(' ') if val != '']
+distances = [int(val) for val in data[1].split(':')[1].split(' ') if val != '']
+races = {time:distance for time, distance in zip(times, distances)}
+ans = 1
+# for race in races:
+for time, distance in zip(times, distances):
+    pos_distances = [i*(time-i) for i in range(time)]
+    push_times = [i for i, pd in enumerate(pos_distances) if pd > distance]
+    ways = len(push_times)
+    ans *= ways
+print(ans)
--- 6/second.py
+++ 6/second.py
@@ -1,2 +1,8 @@
-f = open('input.txt', 'r')
+f = open('test.txt', 'r')
 data = [row.strip() for row in f]
+time = int(''.join([val for val in data[0].split(':')[1].split(' ') if val != '']))
+distance = int(''.join([val for val in data[1].split(':')[1].split(' ') if val != '']))
+pos_distances = [i*(time-i) for i in range(time)]
+push_times = [i for i, pd in enumerate(pos_distances) if pd > distance]
+ways = len(push_times)
+print(ways)
